98f81ba0beaa5d23f9b250b3bfb689f6e9d2bf8c
asepabdaz <asep.abdaz@cpssoft.com>
2021-05-10 23:09:06 +0700

    temp

--- LargeProjectTests/LargeProjectTests.swift
+++ LargeProjectTests/LargeProjectTests.swift
@@ -20,6 +20,9 @@ class LargeProjectTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+        let i = 0
+        XCTAssertEqual(i, 0, "match")
     }
 
     func testPerformanceExample() throws {
